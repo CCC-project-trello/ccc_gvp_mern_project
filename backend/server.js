60effa07ea5ed1420d58fff82efae8713d5afe00
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-04-27 15:34:11 +0530

    Add .gitignore to exclude venv

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
